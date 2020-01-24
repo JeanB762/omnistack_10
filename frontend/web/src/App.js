bf4217a29b663eb0b0e2550ef72ba13a7db36871
Jean Borges <jeanborges946@gmail.com>
2020-01-24 01:32:20 -0300

    Finalizaçao da interface

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
+import './main.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+          </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/18680754?s=460&v=4" alt="Jean Borges" />
+              <div className="user-info">
+                <strong>Jean Borges</strong>              
+                <span>ReactJs, ReactNative, NodeJs</span>
+              </div>
+            </header>
+            <p>Programador de bosta web e mobile.</p>
+            <a href="https://github.com/JeanB762">Acessar no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/18680754?s=460&v=4" alt="Jean Borges" />
+              <div className="user-info">
+                <strong>Jean Borges</strong>              
+                <span>ReactJs, ReactNative, NodeJs</span>
+              </div>
+            </header>
+            <p>Programador de bosta web e mobile.</p>
+            <a href="https://github.com/JeanB762">Acessar no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/18680754?s=460&v=4" alt="Jean Borges" />
+              <div className="user-info">
+                <strong>Jean Borges</strong>              
+                <span>ReactJs, ReactNative, NodeJs</span>
+              </div>
+            </header>
+            <p>Programador de bosta web e mobile.</p>
+            <a href="https://github.com/JeanB762">Acessar no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/18680754?s=460&v=4" alt="Jean Borges" />
+              <div className="user-info">
+                <strong>Jean Borges</strong>              
+                <span>ReactJs, ReactNative, NodeJs</span>
+              </div>
+            </header>
+            <p>Programador de bosta web e mobile.</p>
+            <a href="https://github.com/JeanB762">Acessar no GitHub</a>
+          </li>
+        </ul>
+      </main>
+
     </div>
   );
 }
